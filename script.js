c48fbb27d452a791bd8f490cc3045041058cdb1c
e227658 <e227658@edu.sbl.ch>
2022-11-16 14:46:05 +0100

    change colour back to green

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function handleKeyPress (ev) {
-const player = document.querySelector("#player")
-player.setAttribute("cx", 100 * Math.random ())
-player.setAttribute("cy", 100 * Math.random ())
+    if (ev.code === "Space") {
+        const player = document.querySelector("#player") ;
+        player.setAttribute("cx", 100 * Math.random ()) ;
+        player.setAttribute("cy", 100 * Math.random ()) ;
+        
+    } else if (ev.key === "r") {
+        player.setAttribute("fill","red")
+    }
+    else if (ev.key === "g") {
+        player.setAttribute("fill","green ")
+    }
 
 } 
 
